32847981aa501ede5c796014afdc872a0e070893
sergiskenderi <sergi.skenderi@idra.al>
2021-06-08 11:42:51 +0200

    stretch goals

--- src/components/FormGroup.js
+++ src/components/FormGroup.js
@@ -3,12 +3,17 @@ import {Button, Form} from "react-bootstrap";
 import "./formGroup.css";
 import * as yup from "yup";
 import axios from "axios";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 const formSchema = yup.object().shape({
     name : yup.string().required("Full Name is required!"),
-    email: yup.string().email("Please enter a valid email").required("Email is required"),
+    email: yup.string().email("Please enter a valid email").required("Email is required").notOneOf(["waffle@syrup.com"] , "That email is already taken"),
     password : yup.string().min(8).required("Password is required"),
     terms: yup.boolean().oneOf([true], "Please agree to terms of use"),
+    role : yup.string().required("Please select the role of user"),
+    country : yup.string().required("Please enter your country"),
+    birthday : yup.date().required("Please enter your birthday"),
+    news: yup.boolean()
 });
 
 function FormGroup(){
@@ -16,14 +21,22 @@ function FormGroup(){
         name : "",
         email : "",
         password : "",
-        terms : false
+        terms : false,
+        role : "",
+        country : "",
+        birthday : "",
+        news : false
     })
 
     const [errors , setErrors] = useState({
         name : "",
         email : "",
         password : "",
-        terms : ""
+        terms : "",
+        role : "",
+        country : "",
+        birthday : "",
+        news : ""
     })
 
     const [users , setUsers] = useState([]);
@@ -89,7 +102,11 @@ function FormGroup(){
                 name : "",
                 email : "",
                 password : "",
-                terms : false
+                terms : false,
+                role : "",
+                country : "",
+                birthday : "",
+                news : false
             });
         }).catch( (error) => {
             console.log(error.res);
@@ -98,6 +115,7 @@ function FormGroup(){
 
     return(
         <div className="mainForm">
+        <AccountCircleIcon className="mainIcon" />
         <Form onSubmit={formSubmit} >
         <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>Name</Form.Label>
@@ -126,6 +144,36 @@ function FormGroup(){
             </Form.Text>
         </Form.Group>
 
+        <Form.Group className="mb-3" controlId="ControlSelect1">
+            <Form.Label>Select Role</Form.Label>
+            <Form.Control as="select" name="role" onChange={fieldChange} value={formState.role}>
+            <option></option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+            <option value="superadmin">SuperAdmin</option>
+            </Form.Control>
+            <Form.Text className="text-muted">
+            {errors.role.length > 0 ? (<p style={{color : 'red'}} >{errors.role}</p>) : null}
+            </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicCountry">
+            <Form.Label>Country</Form.Label>
+            <Form.Control type="text" name="country" placeholder="Enter country" value={formState.country}
+            onChange={fieldChange}/>
+            <Form.Text className="text-muted">
+            {errors.country.length > 0 ? (<p style={{color : 'red'}} >{errors.country}</p>) : null}
+            </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicDate">
+            <Form.Label>Birthday</Form.Label>
+            <Form.Control type="date" name="birthday" placeholder="Enter birthday" value={formState.birthday}
+            onChange={fieldChange}/>
+            <Form.Text className="text-muted">
+            {errors.birthday.length > 0 ? (<p style={{color : 'red'}} >{errors.birthday}</p>) : null}
+            </Form.Text>
+        </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" name="terms" label="Terms and Conditions" checked={formState.terms}
@@ -134,12 +182,21 @@ function FormGroup(){
             {errors.terms.length > 0 ? (<p style={{color : 'red'}} >{errors.terms}</p>) : null}
             </Form.Text>
         </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicCheckbox1">
+            <Form.Check type="checkbox" name="news" label="Send Email about latest News" checked={formState.news}
+            onChange={fieldChange}/>
+            <Form.Text className="text-muted">
+            {errors.news.length > 0 ? (<p style={{color : 'red'}} >{errors.news}</p>) : null}
+            </Form.Text>
+        </Form.Group>
+
         <Button variant="primary" type="submit" disabled={disabled}>
             Submit
         </Button>
         {users.map( user => {
         return (
-        <React.Fragment>
+        <React.Fragment key={user.createdAt}>
         <Form.Group className="mb-3" controlId="formBasicResponse">
                 <Form.Text className="text-muted" style={{marginTop: '3px'}}>
                 {JSON.stringify(user, null, 5)}
